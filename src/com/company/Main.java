e6a7e2004be043cb910e0b7697cb150263022bfc
jhlahey2 <jhlahey@protonmail.com>
2018-10-03 17:12:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+/***********************************************************
+ * Assignment
+ * Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable named myothernumber,
+ * and then prints out the values to the console.
+ */
+public class Main {
+
+    public static void main(String [] args)
+    {
+        int mynumber = 2;
+        double myothernumber   = 1.7938;
+
+        System.out.println("mynumber is " + mynumber);
+        System.out.println("myothernumber is " + myothernumber);
+    }
+}
